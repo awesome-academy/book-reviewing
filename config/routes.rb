e0840456fa943a950e4d0ccefe3dcef256c8036f
Hoa Duong <tahado98@gmail.com>
2019-07-30 11:08:59 +0700

    Create books pages 2
    Create books pages 2

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  scope "(:locale)", locale: /en|vi/ do
+    root "static_pages#home"
+
+    get "/help", to: "static_pages#help"
+    get "/about", to: "static_pages#about"
+    get "/signup", to: "users#new"
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+
+    resources :users
+    resources :books
+  end
+end
